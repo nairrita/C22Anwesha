437753d8da474e7da09792811e1d6009704f494e
nairrita <53215055+nairrita@users.noreply.github.com>
2020-04-01 12:43:56 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# C22Anwesha
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,31 @@
+const World = Matter.World;
+const Engine = Matter.Engine;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var ground;
+
+
+function setup() {
+  createCanvas(400,400);
+  engine = Engine.create();
+  world = engine.world;
+
+  var ground_options={
+    isStatic : true
+  }
+
+
+  ground =  Bodies.rectangle(200,380,400,20,ground_options)
+
+  World.add(world,ground)
+}
+
+function draw() {
+  background(0);  
+ Engine.update(engine)
+
+  rectMode(CENTER)
+  rect(ground.position.x,ground.position.y,400,20)
+ 
+}
